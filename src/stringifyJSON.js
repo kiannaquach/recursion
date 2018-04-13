3b8fc1268474ab51f540906cb1eff7771cd9c25a
Kianna <kiannaquach.kq@gmail.com>
2018-04-12 19:03:48 -0700

    Fixed stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,38 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'number') {
+  	return '' + obj;
+  }
+
+  if (obj === null) {
+  	return 'null';
+  }
+
+  if (typeof obj === 'boolean') {
+  	return obj + '';
+  }
+
+  if (typeof obj === 'string') {
+  	return '"' + obj + '"';
+  }
+
+  var stringifiedArray = [];
+  var stringifiedObj = [];
+
+  if (Array.isArray(obj)) {
+  	for (var i = 0; i < obj.length; i++) {
+  		stringifiedArray.push(stringifyJSON(obj[i])); // stringifies every el in arr according to base cases
+  		// console.log(stringifiedArray)
+  	}
+  	return '[' + stringifiedArray + ']';
+  } else {
+  	for (var key in obj) {
+  		if (typeof obj[key] !== 'undefined' && typeof obj[key] !== 'function') { 
+  			stringifiedObj.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+  			// console.log(stringifiedObj)
+  		}
+  	}
+  	return '{' + stringifiedObj + '}';
+  }
 };
