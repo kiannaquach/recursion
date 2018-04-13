27898573a787c759cc978c11c8aa82842e945bf4
Kianna <kiannaquach.kq@gmail.com>
2018-04-12 19:03:20 -0700

    fixed getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,48 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+	// element.classList
+	// document.body
+	// element.childNodes
+	var results = [];
+	var getElements = function(element) {
+		if (element.classList && element.classList.contains(className)) {
+			results.push(element);
+		}
+
+		if (element.childNodes) {
+			_.each(element.childNodes,function(childNodeElement) {
+				getElements(childNodeElement);
+			})
+		}
+	}
+
+	getElements(document.body); // invoke inner function getElements on the document.body
+	return results; // return the results
 };
+/*
+[
+  '<div class="targetClassName"></div>',
+  '<div class="otherClassName targetClassName"></div>',
+  '<div>
+  	<div class="targetClassName"></div>
+  </div>',
+  '<div>
+	  <div class="targetClassName">
+	  	<div class="targetClassName">
+	  	</div>
+	  </div>
+   </div>',
+  '<div>
+  	<div>
+  	</div>
+  		<div>
+  			<div class="targetClassName">
+  			</div>
+  		</div>
+  </div>',
+  '<div><div class="targetClassName"></div><div class="targetClassName"></div></div>',
+  '<div><div class="somediv"><div class="innerdiv"><span class="targetClassName">yay</span></div></div></div>'
+]
+*/
